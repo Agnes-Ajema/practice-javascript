6d56e7ef20d20be8d3b4bf31d86368e525a877e4
Agnes <agnesajema@gmail.com>
2024-03-18 21:00:48 +0300

    add files

---
+++ javascript.js
@@ -0,0 +1,36 @@
+let a = "Hello"
+let b = "World"
+console.log((a+" "+b));
+
+let firstName = "Agnes"
+b = "Agnes"
+let lastName = "Ajema"
+c = "Ajema"
+d = "Agnes Ajema"
+console.log((d));
+
+let e = "The current year is long"
+let f = 2024
+let g = `The current year ${f} is long`
+console.log((g));
+
+let h = "I am years old"
+let i = 23
+let j = `I am ${i} years old`
+console.log((j));
+
+let k = "javascript"
+console.log(k.charAt(0));
+console.log(k.charAt(9));
+
+let l = ["mangoes" ,"apples","oranges", "berrys","pine"]
+console.log(l[2])
+
+let num1 = 25 
+let num2 = 24
+console.log(num1+num2);
+
+let quantity = 60
+let price = 100
+let cost = (quantity*price)
+console.log(cost)
